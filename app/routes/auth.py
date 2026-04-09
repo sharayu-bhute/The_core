575034d20fe3394a0e6502a7663bdd4787a2e367
sharayu_bhute <sharayubhute22@gmail.com>
2026-04-09 13:31:57 +0530

    sign_up from

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,23 +1,26 @@
 from flask import Blueprint,request,redirect,url_for,render_template,flash,session
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email
-
+from app.form import RegistrationForm
 auth_bp=Blueprint('auth',__name__)
 USER_CREDENTIALS={'username':'admin','password':'1234'}
 
 
 @auth_bp.route('/login',methods=['GET','POST'])
 def login():
-    if request.method=='POST':
-        username=request.form.get("username")
-        email=request.form.get("email",Datarequired)
-        password=request.form.get("password")
-        if username==USER_CREDENTIALS['username'] and password==USER_CREDENTIALS['password']:
-            session['user']=username
-            flash('login successful','success')
+    form = RegistrationForm()
+    if form.validate_on_submit():  
+        username = form.name.data
+        email = form.email.data
+        password = form.password.data
+        if username == USER_CREDENTIALS['username'] and password == USER_CREDENTIALS['password']:
+            session['user'] = username
+            flash('Login successful', 'success')
+            return redirect(url_for('auth.login'))
         else:
-            flash('invalid credentials','danger')   
-    return render_template('login.html')
+            flash('Invalid credentials', 'danger')
+
+    return render_template('login.html', form=form)
 
 
 @auth_bp.route('/logout')
@@ -25,3 +28,6 @@ def logout():
     session.pop("user",None)
     flash('logout successful','success')
     return redirect(url_for('auth.login'))
+
+
+
